e28c04cf6b0a03306a285c672f22e42d7203f42f
Vaibhav Srivastava <vaibhav.12may95@gmail.com>
2019-08-21 20:54:38 +0530

    make upper half view mobile responsive

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,11 +1,13 @@
     
-$('.btn').on('click', function(){
-  alert('hi')
+// $('.btn').on('click', function(){
+//   alert('hi')
+// })
+$ ('#mobile-development-view').on('click', function(){
+  window.location.replace('/view')
 })
-
+$('#bolt-logo-b').hide()
 var target = $('#navbar2').offset().top,
     timeout = null;
-
 $(window).scroll(function () {
     if (!timeout) {
         timeout = setTimeout(function () {            
@@ -14,8 +16,10 @@ $(window).scroll(function () {
             if ($(window).scrollTop() >= target) {
               $('#menu').hide()
               $('#navbar2').addClass('nav2-on-top')
+              $('#bolt-logo-b').show()
             } else{
               $('#menu').show()
+              $('#bolt-logo-b').hide()
               $('#navbar2').removeClass('nav2-on-top')
             }
         }, 250);
