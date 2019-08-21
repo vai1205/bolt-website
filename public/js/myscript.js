6a652068c7703dcd71e7d0611e7ad927def2bffb
Vaibhav Srivastava <vaibhav.12may95@gmail.com>
2019-08-21 20:58:41 +0530

    merge problem resolve

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,4 +1,27 @@
     
-$('.btn').on('click', function(){
-  alert('hi')
+// $('.btn').on('click', function(){
+//   alert('hi')
+// })
+$ ('#mobile-development-view').on('click', function(){
+  window.location.replace('/view')
 })
+$('#bolt-logo-b').hide()
+var target = $('#navbar2').offset().top,
+    timeout = null;
+$(window).scroll(function () {
+    if (!timeout) {
+        timeout = setTimeout(function () {            
+            clearTimeout(timeout);
+            timeout = null;
+            if ($(window).scrollTop() >= target) {
+              $('#menu').hide()
+              $('#navbar2').addClass('nav2-on-top')
+              $('#bolt-logo-b').show()
+            } else{
+              $('#menu').show()
+              $('#bolt-logo-b').hide()
+              $('#navbar2').removeClass('nav2-on-top')
+            }
+        }, 250);
+    }
+});
