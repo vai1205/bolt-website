b90e365f63c8e09e7fa4785b9d91d9c4c579f3f0
Vaibhav Srivastava <vaibhav.12may95@gmail.com>
2019-08-21 07:54:57 +0530

    change navbars on scroll on view template

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -2,3 +2,22 @@
 $('.btn').on('click', function(){
   alert('hi')
 })
+
+var target = $("#navbar2").offset().top,
+    timeout = null;
+
+$(window).scroll(function () {
+    if (!timeout) {
+        timeout = setTimeout(function () {            
+            clearTimeout(timeout);
+            timeout = null;
+            if ($(window).scrollTop() >= target) {
+              $('#menu').hide()
+              $('#navbar2').addClass('nav2-on-top')
+            } else{
+              $('#menu').show()
+              $('#navbar2').removeClass('nav2-on-top')
+            }
+        }, 250);
+    }
+});
